de9f5f6669bb87d50ea1fd10d403d5cdf3fb82e2
Thomas Faller <tfaller1@gmx.de>
2025-04-28 20:18:44 +0200

    OpenApiGenerator: General generator infrastructure

---
+++ src/TFaller.ALTools.OpenApiGenerator/src/IGenerator.cs
@@ -0,0 +1,17 @@
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace TFaller.ALTools.OpenApiGenerator;
+
+public interface IGenerator
+{
+    /// <summary>
+    /// Method generats AL code for the given schema.
+    /// </summary>
+    /// <param name="code">The generated code</param>
+    /// <param name="name">Property/Schema name</param>
+    /// <param name="schema">The schema</param>
+    /// <param name="required">Whether the property is required</param>
+    /// <returns>Status about what code was generated.</returns>
+    public GenerationStatus GenerateCode(StringBuilder code, string name, OpenApiSchema schema, bool required);
+}
