8cf5382d641c8631788603192d6799fffaa75774
Thomas Faller <tfaller1@gmx.de>
2025-05-01 21:40:23 +0200

    TransformInlineTypesTests: Format code

--- test/TFaller.ALTools.OpenApiGenerator.Tests/src/TransformInlineTypesTests.cs
+++ test/TFaller.ALTools.OpenApiGenerator.Tests/src/TransformInlineTypesTests.cs
@@ -4,28 +4,28 @@ namespace TFaller.ALTools.OpenApiGenerator.Tests;
 
 public class TransformInlineTypesTests
 {
-  [Fact]
-  public void TransformArrayObject()
-  {
-    var doc = ParseYamlOpenApiDocument(
-      """
-      openapi: 3.1.0
-      info:
-        title: Test
-        version: 1
-      components:
-        schemas:
-          list:
-            type: array
-            items:
-              type: object
-      """);
+    [Fact]
+    public void TransformArrayObject()
+    {
+        var doc = ParseYamlOpenApiDocument(
+            """
+            openapi: 3.1.0
+            info:
+              title: Test
+              version: 1
+            components:
+              schemas:
+                list:
+                  type: array
+                  items:
+                    type: object
+            """);
 
-    TransformInlineTypes.Transform(doc);
+        TransformInlineTypes.Transform(doc);
 
-    var schemas = doc.Components?.Schemas;
-    Assert.NotNull(schemas);
-    Assert.Contains("list", schemas);
-    Assert.Contains("listItem", schemas);
-  }
+        var schemas = doc.Components?.Schemas;
+        Assert.NotNull(schemas);
+        Assert.Contains("list", schemas);
+        Assert.Contains("listItem", schemas);
+    }
 }
