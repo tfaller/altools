bdf6f503e4495a8dd1fd857902bf23775c7ea456
Thomas Faller <tfaller1@gmx.de>
2026-01-09 21:56:50 +0100

    AppPackageHelper: WriteManifest

--- src/TFaller.ALTools.Transformation/src/AppPackageHelper.cs
+++ src/TFaller.ALTools.Transformation/src/AppPackageHelper.cs
@@ -1,5 +1,6 @@
 using Microsoft.Dynamics.Nav.CodeAnalysis.Packaging;
 using Microsoft.Dynamics.Nav.CodeAnalysis.SymbolReference;
+using System;
 using System.IO;
 
 namespace TFaller.ALTools.Transformation;
@@ -12,7 +13,35 @@ public static class AppPackageHelper
     public static void WriteModule(this NavAppPackage app, ModuleDefinition moduleDefinition)
     {
         using var writer = new NavAppPackageWriter(app, FileMode.Open);
+        writer.WriteModule(moduleDefinition);
+    }
+
+    public static void WriteModule(this NavAppPackageWriter writer, ModuleDefinition moduleDefinition)
+    {
         using var stream = writer.GetPartStream("/SymbolReference.json");
         SymbolReferenceJsonWriter.WriteModule(stream, moduleDefinition);
+    }
+
+    public static void WriteManifest(this NavAppPackageWriter writer, NavAppManifest manifest)
+    {
+        ArgumentNullException.ThrowIfNull(manifest, nameof(manifest));
+
+        // The following checks make sure we write a manifest that actually is valid and can be read by other tools
+
+        ArgumentNullException.ThrowIfNull(manifest.AppVersion, nameof(manifest) + "." + nameof(manifest.AppVersion));
+
+        if (manifest.AppId == default)
+        {
+            // Technically other tools might accept this, but it could break if multiple apps use the empty GUID
+            throw new ArgumentException("AppId must be set in the manifest before writing it to a package.", nameof(manifest));
+        }
+
+        if (manifest.AppCompatibilityId == null)
+        {
+            // Don't throw here, this is kind of a special case. Use the default version
+            manifest.AppCompatibilityId = new Version(0, 0, 0, 0);
+        }
+
+        writer.WriteString(manifest.ToXml(), "/NavxManifest.xml");
     }
 }
